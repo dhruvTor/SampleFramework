d7f610ffa381dad18fc411f8564c479934b5c9f6
dhruv <dhruvjaiswal5@gmail.com>
2022-08-05 01:19:16 +0530

    network public api added

--- Sources/SampleFramework/Networking.swift
+++ Sources/SampleFramework/Networking.swift
@@ -9,5 +9,26 @@ import Foundation
 
 extension SampleFramework {
     public class Networking{
+        
+        /// Responsible for handling all networking calls
+        /// - Warning: Must create before any API call
+        public class Manager{
+            public init(){}
+            
+            private var session = URLSession.shared
+            
+            public func loadData(from url:URL,completionHandler:@escaping (NetworkResult<Data>) -> Void){
+               let task = session.dataTask(with: url) { data, response, error in
+                   let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                   completionHandler(result)
+                }
+                task.resume()
+            }
+        }
+    }
+    
+    public enum NetworkResult<Value>{
+        case success(Value)
+        case failure(Error?)
     }
 }
--- Tests/SampleFrameworkTests/SampleFrameworkNetworkTest.swift
+++ Tests/SampleFrameworkTests/SampleFrameworkNetworkTest.swift
@@ -10,8 +10,22 @@ import XCTest
 
 final class SampleFrameworkNetworkTest: XCTestCase {
 
-    func testExample() throws {
-  
+    func testLoadCallData() {
+        let manager = SampleFramework.Networking.Manager()
+        let exp = XCTestExpectation(description: "called for data")
+        guard let url = URL(string: "https://raywenderlich.com") else{
+            return XCTFail("Could not create url properly")
+        }
+        manager.loadData(from:url){ result in
+            exp.fulfill()
+            switch result{
+            case .success(let returnedData):
+                XCTAssertNotNil(returnedData)
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error")
+            }
+        }
+        wait(for: [exp], timeout: 5)
     }
 
 
